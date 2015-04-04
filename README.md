994793c2a4baa7298359e3e5becbb908072a6987
Fernando Alexandre <fernando-alexandre@Fernandos-MacBook-Pro.local>
2015-04-03 22:50:08 -0300

    v0.1 simple fileuptload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pytos3
+
+### TODO
+- 4GB > files
+- Set parameters to get filename and bucket name
+
+### Needed:
+boto
--- pytos3.py
+++ pytos3.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
+#Import general
 import sys
 import ConfigParser, os
 
+#Import boto
+import boto
+from boto.s3.key import Key
+
 def load_conf(conf_file):
 
-    conf_path = conf_path = os.path.dirname(os.path.realpath(__file__)) + conf_file
+    conf_path = os.path.dirname(os.path.realpath(__file__)) + conf_file
 
     config = ConfigParser.RawConfigParser()
     config.readfp(open(conf_path))
@@ -17,8 +22,21 @@ def load_conf(conf_file):
 
     return parameters
 
+def put_in_bucket(file):
+
+    file_path = os.path.dirname(os.path.realpath(__file__)) + file
+
+    parameters = load_conf("/pytos3.conf")
+
+    conn = boto.connect_s3(parameters['AWS_ACCESS_KEY_ID'], parameters['AWS_SECRET_ACCESS_KEY'])
+
+    #Create a key to word with s3
+    s3_key = Key(conn.get_bucket('pytos3-backup'))
+    s3_key.key = ('teste.txt')
+    s3_key.set_contents_from_filename(file_path)
+
 def main():
-    print load_conf("/pytos3.conf")
+    put_in_bucket("/teste1.txt")
 
 if __name__ == "__main__":
     main()
