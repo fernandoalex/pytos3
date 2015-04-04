3c04c01e01fc974bbaf72d9cfde5f37482fe2b84
Fernando Alexandre <fernando-alexandre@Fernandos-MacBook-Pro.local>
2015-04-03 22:15:03 -0300

    load_conf

--- pytos3.py
+++ pytos3.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 
+import sys
+import ConfigParser, os
 
-def test :
-    return
+def load_conf(conf_file):
+
+    conf_path = conf_path = os.path.dirname(os.path.realpath(__file__)) + conf_file
+
+    config = ConfigParser.RawConfigParser()
+    config.readfp(open(conf_path))
+
+    parameters = {
+        'AWS_ACCESS_KEY_ID': config.get("credentials", "AWS_ACCESS_KEY_ID"),
+        'AWS_SECRET_ACCESS_KEY':config.get("credentials", "AWS_SECRET_ACCESS_KEY"),
+    }
+
+    return parameters
+
+def main():
+    print load_conf("/pytos3.conf")
 
 if __name__ == "__main__":
     main()
